fe5e8e24a46b5a1d4cfd6b57be480a235538c476
hemnekapil <hemnekapil24@gmail.com>
2025-11-25 20:49:24 +0530

    EnumTypealias

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+enum ServerResponseStatus {
+  Success = 200,
+  Error = 500,
+}
+
+Object.values(ServerResponseStatus).forEach((value) => {
+  if (typeof value === "number") {
+    console.log(value);
+  }
+});
+
+console.log(ServerResponseStatus);
+
+interface ServerResponse {
+  result: ServerResponseStatus;
+  data: string[];
+}
+function getServerresponse(): ServerResponse {
+  return {
+    result: ServerResponseStatus.Success,
+    data: ["first item", "second item"],
+  };
+}
+const response: ServerResponse = getServerresponse();
+console.log(response);
