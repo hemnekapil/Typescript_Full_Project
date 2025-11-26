2d130752c1d45bb4aebb90199a28d9b67752207b
hemnekapil <hemnekapil24@gmail.com>
2025-11-26 22:58:34 +0530

    GenricType

--- src/fundamentals/GenricTypes.ts
+++ src/fundamentals/GenricTypes.ts
@@ -1,57 +1,83 @@
-function getById<T extends { id: number }>(
-  items: T[],
-  id: number
-): T | undefined {
-  return items.find((item) => item.id === id);
-}
+// function getById<T extends { id: number }>(
+//   items: T[],
+//   id: number
+// ): T | undefined {
+//   return items.find((item) => item.id === id);
+// }
+
+// interface TestUser {
+//   id: number;
+//   username: string;
+//   email: string;
+// }
+// interface TestProduct {
+//   id: number;
+//   name: string;
+//   price: number;
+// }
+
+// const users: TestUser[] = [
+//   { id: 1, username: "kpie", email: "kpie753@test.com" },
+//   { id: 2, username: "rancho", email: "rancho753@test.com" },
+//   { id: 3, username: "kapil", email: "kapil753@test.com" },
+// ];
+
+// const products: TestProduct[] = [
+//   { id: 101, name: "Laptop", price: 25000 },
+//   { id: 102, name: "Mobile", price: 20000 },
+//   { id: 103, name: "TV", price: 250000 },
+// ];
+// const foundUser = getById(users, 2);
+// //console.log(foundUser);
+// const findProduct = getById(products, 103);
+// //console.log(findProduct);
+
+// interface TestResponse<T> {
+//   success: boolean;
+//   data: T;
+//   error?: string;
+// }
+// function createSuccessResponse<T>(data: T): TestResponse<T> {
+//   return { success: true, data: data };
+// }
+// function createErrorResponse<T>(errorMessage: string): TestResponse<T> {
+//   return { success: false, data: null as unknown as T, error: errorMessage };
+// }
+// const userReponse = createSuccessResponse<TestUser[]>(users);
+// console.log("Users Array response:", userReponse);
+// const errorReponse = createErrorResponse("User not found");
+// console.log(errorReponse);
+// //
+// function mapToIds<T extends { id: number }>(items: T[]): number[] {
+//   return items.map((item) => item.id);
+// }
+// const userDataWithId = mapToIds(users);
+// console.log(userDataWithId);
+// const productDataWithId = mapToIds(products);
+// console.log(productDataWithId);
 
 interface TestUser {
   id: number;
   username: string;
   email: string;
-}
-interface TestProduct {
-  id: number;
-  name: string;
-  price: number;
+  role: "admin" | "user";
 }
 
-const users: TestUser[] = [
-  { id: 1, username: "kpie", email: "kpie753@test.com" },
-  { id: 2, username: "rancho", email: "rancho753@test.com" },
-  { id: 3, username: "kapil", email: "kapil753@test.com" },
-];
+//genric function with keyof constraints
+//T[K] means type of property key on T
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key]; // Testuser[id]
+}
 
-const products: TestProduct[] = [
-  { id: 101, name: "Laptop", price: 25000 },
-  { id: 102, name: "Mobile", price: 20000 },
-  { id: 103, name: "TV", price: 250000 },
-];
-const foundUser = getById(users, 2);
-//console.log(foundUser);
-const findProduct = getById(products, 103);
-//console.log(findProduct);
+const user1: TestUser = {
+  id: 1,
+  username: "kapil",
+  email: "kpie@gmail.com",
+  role: "admin",
+};
 
-interface TestResponse<T> {
-  success: boolean;
-  data: T;
-  error?: string;
-}
-function createSuccessResponse<T>(data: T): TestResponse<T> {
-  return { success: true, data: data };
-}
-function createErrorResponse<T>(errorMessage: string): TestResponse<T> {
-  return { success: false, data: null as unknown as T, error: errorMessage };
-}
-const userReponse = createSuccessResponse<TestUser[]>(users);
-console.log("Users Array response:", userReponse);
-const errorReponse = createErrorResponse("User not found");
-console.log(errorReponse);
-//
-function mapToIds<T extends { id: number }>(items: T[]): number[] {
-  return items.map((item) => item.id);
-}
-const userDataWithId = mapToIds(users);
-console.log(userDataWithId);
-const productDataWithId = mapToIds(products);
-console.log(productDataWithId);
+const u1 = getProperty(user1, "username");
+const u2 = getProperty(user1, "id");
+//const u3 = getProperty(user1, "id1"); not assinable type parameter
+console.log(u1);
+console.log(u2);
--- src/fundamentals/ProductInterfaces.ts
+++ src/fundamentals/ProductInterfaces.ts
@@ -1,104 +1,154 @@
-//interface
-interface Book {
-  readonly isbn: number;
-  title: string;
-  author: string;
-  genere?: string;
-  printAuthor(): void;
-  printTitle(message: string): string;
+// //interface
+// interface Book {
+//   readonly isbn: number;
+//   title: string;
+//   author: string;
+//   genere?: string;
+//   printAuthor(): void;
+//   printTitle(message: string): string;
+// }
+
+// const deepWork: Book = {
+//   isbn: 123,
+//   title: "deep work",
+//   author: "cal newport",
+//   genere: "self help",
+//   printAuthor() {
+//     console.log(this.author);
+//   },
+//   printTitle(message) {
+//     return `${this.title} ${message}`;
+//   },
+// };
+
+// deepWork.printAuthor();
+// const resultTitle = deepWork.printTitle("This is awesomebook");
+// console.log(resultTitle);
+// interface Computer {
+//   readonly id: number;
+//   brand: string;
+//   ram: number;
+//   upgradeRam(increase: number): number;
+//   storage?: number;
+// }
+
+// const laptop: Computer = {
+//   id: 101,
+//   brand: "lenovo",
+//   ram: 16,
+//   upgradeRam(amount) {
+//     this.ram += amount;
+//     return this.ram;
+//   },
+// };
+
+// laptop.storage = 256;
+// console.log(laptop.upgradeRam(20));
+
+// console.log(laptop);
+// interface Person {
+//   name: string;
+//   getDetails(): string;
+// }
+
+// interface Person {
+//   age: number;
+// }
+
+// interface DogOwner {
+//   dogName: string;
+//   getDogDetails(): string;
+// }
+
+// const person: Person = {
+//   name: "John",
+//   age: 30,
+//   getDetails() {
+//     return `Name:${this.name}, Age:${this.age}`;
+//   },
+// };
+
+// interface Employee1 extends Person {
+//   employeeId: number;
+// }
+
+// const employee: Employee1 = {
+//   name: "jane",
+//   employeeId: 123,
+//   age: 30,
+//   getDetails() {
+//     return `Name:${this.name}, Age:${this.age}, Employeed ID:${this.employeeId}`;
+//   },
+// };
+
+// console.log(employee.getDetails());
+
+// interface Manager1 extends Person, DogOwner {
+//   managePeople(): void;
+// }
+
+// const manager1: Manager1 = {
+//   name: "Bob",
+//   age: 35,
+//   dogName: "Rex",
+//   getDetails() {
+//     return `Name:${this.name}, Age:${this.age}`;
+//   },
+//   getDogDetails() {
+//     return `Name:${this.dogName}`;
+//   },
+//   managePeople() {
+//     console.log("Managing people");
+//   },
+// };
+// manager1.managePeople();
+
+// enum ServerResponseStatus {
+//   Success = 200,
+//   Error = 500,
+// }
+
+// Object.values(ServerResponseStatus).forEach((value) => {
+//   if (typeof value === "number") {
+//     console.log(value);
+//   }
+// });
+
+// console.log(ServerResponseStatus);
+
+// interface ServerResponse {
+//   result: ServerResponseStatus;
+//   data: string[];
+// }
+// function getServerresponse(): ServerResponse {
+//   return {
+//     result: ServerResponseStatus.Success,
+//     data: ["first item", "second item"],
+//   };
+// }
+// const response: ServerResponse = getServerresponse();
+// console.log(response);
+
+enum UserRole {
+  Admin,
+  Manager,
+  Employee,
 }
-
-const deepWork: Book = {
-  isbn: 123,
-  title: "deep work",
-  author: "cal newport",
-  genere: "self help",
-  printAuthor() {
-    console.log(this.author);
-  },
-  printTitle(message) {
-    return `${this.title} ${message}`;
-  },
-};
-
-deepWork.printAuthor();
-const resultTitle = deepWork.printTitle("This is awesomebook");
-console.log(resultTitle);
-interface Computer {
-  readonly id: number;
-  brand: string;
-  ram: number;
-  upgradeRam(increase: number): number;
-  storage?: number;
-}
-
-const laptop: Computer = {
-  id: 101,
-  brand: "lenovo",
-  ram: 16,
-  upgradeRam(amount) {
-    this.ram += amount;
-    return this.ram;
-  },
-};
-
-laptop.storage = 256;
-console.log(laptop.upgradeRam(20));
-
-console.log(laptop);
-interface Person {
+type User3 = {
+  id: number;
   name: string;
-  getDetails(): string;
-}
-
-interface Person {
-  age: number;
-}
-
-interface DogOwner {
-  dogName: string;
-  getDogDetails(): string;
-}
-
-const person: Person = {
-  name: "John",
-  age: 30,
-  getDetails() {
-    return `Name:${this.name}, Age:${this.age}`;
-  },
+  role: UserRole;
+  contact: [string, string];
 };
 
-interface Employee1 extends Person {
-  employeeId: number;
+function createUser(user: User3): User3 {
+  return user;
 }
 
-const employee: Employee1 = {
-  name: "jane",
-  employeeId: 123,
-  age: 30,
-  getDetails() {
-    return `Name:${this.name}, Age:${this.age}, Employeed ID:${this.employeeId}`;
-  },
-};
-
-console.log(employee.getDetails());
-
-interface Manager1 extends Person, DogOwner {
-  managePeople(): void;
-}
-
-const manager1: Manager1 = {
-  name: "Bob",
-  age: 35,
-  dogName: "Rex",
-  getDetails() {
-    return `Name:${this.name}, Age:${this.age}`;
-  },
-  getDogDetails() {
-    return `Name:${this.dogName}`;
-  },
-  managePeople() {
-    console.log("Managing people");
-  },
-};
-manager1.managePeople();
+const user3: User3 = createUser({
+  id: 1,
+  name: "Kpie",
+  role: UserRole.Admin,
+  contact: ["Kpie@gmail.com", "123-456-789"],
+});
+console.log(user3);
