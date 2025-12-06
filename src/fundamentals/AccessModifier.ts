fdf93145360d4361bd5eddd58ac09ec9a77c47ae
hemnekapil <hemnekapil24@gmail.com>
2025-12-06 15:41:16 +0530

    AcceessModifer

---
+++ src/fundamentals/AccessModifier.ts
@@ -0,0 +1,46 @@
+// class Vehicle {
+//   protected brand: string;
+//   constructor(brand: string) {
+//     this.brand = brand;
+//   }
+//   protected start() {
+//     console.log(`${this.brand} is starting...`);
+//   }
+// }
+// class Car extends Vehicle {
+//   model: string;
+//   constructor(brand: string, model: string) {
+//     super(brand);
+//     this.model = model;
+//   }
+//   public showDetails(): void {
+//     console.log(`Brand: ${this.brand}, Model:${this.model}`);
+//     this.start();
+//   }
+// }
+
+// const car = new Car("Toyota", "Corolla");
+// car.showDetails();
+// //car.start();
+// //
+class Vehicle {
+  private enginNo: string;
+  constructor(enginNo: string) {
+    this.enginNo = enginNo;
+  }
+  private showEnginNo(): void {
+    console.log(`Engine no: ${this.enginNo}`);
+  }
+  public displayInfo(): void {
+    this.showEnginNo();
+  }
+}
+class Car extends Vehicle {
+  constructor(engineNo: string) {
+    super(engineNo);
+  }
+  public tryAccess(): void {}
+}
+
+const c = new Car("ENG1245");
+c.displayInfo();
--- src/fundamentals/MultiLevelInhertiance.ts
+++ src/fundamentals/MultiLevelInhertiance.ts
@@ -1,36 +1,36 @@
-class Vehicle {
-  brand: string;
-  constructor(brand: string) {
-    this.brand = brand;
-  }
-  start() {
-    console.log(`${this.brand} is starting...`);
-  }
-}
-class Car extends Vehicle {
-  model: string;
-  constructor(brand: string, model: string) {
-    super(brand);
-    this.model = model;
-  }
-  //explicite override
-  override start(): void {
-    super.start(); //call parent method
-    console.log(`${this.brand} ${this.model} is ready to go`);
-  }
-}
+// class Vehicle {
+//   brand: string;
+//   constructor(brand: string) {
+//     this.brand = brand;
+//   }
+//   start() {
+//     console.log(`${this.brand} is starting...`);
+//   }
+// }
+// class Car extends Vehicle {
+//   model: string;
+//   constructor(brand: string, model: string) {
+//     super(brand);
+//     this.model = model;
+//   }
+//   //explicite override
+//   override start(): void {
+//     super.start(); //call parent method
+//     console.log(`${this.brand} ${this.model} is ready to go`);
+//   }
+// }
 
-class ElectricalCar extends Car {
-  batteryLevel: number;
-  constructor(brand: string, model: string, batteryLevel: number) {
-    super(brand, model);
-    this.batteryLevel = batteryLevel;
-  }
-  override start(): void {
-    super.start();
-    console.log(`Battery at ${this.batteryLevel}%. Ready to drive silently`);
-  }
-}
-//Test
-const myCar = new ElectricalCar("Tesla", "Model 3", 82);
-myCar.start();
+// class ElectricalCar extends Car {
+//   batteryLevel: number;
+//   constructor(brand: string, model: string, batteryLevel: number) {
+//     super(brand, model);
+//     this.batteryLevel = batteryLevel;
+//   }
+//   override start(): void {
+//     super.start();
+//     console.log(`Battery at ${this.batteryLevel}%. Ready to drive silently`);
+//   }
+// }
+// //Test
+// const myCar = new ElectricalCar("Tesla", "Model 3", 82);
+// myCar.start();
